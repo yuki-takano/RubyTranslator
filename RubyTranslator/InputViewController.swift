a2a963ff5bc5a8d7ecd5c3ff1281df40ffd831c8
ytakano <pmapers@gmail.com>
2020-03-14 17:38:09 +0900

    空のプロジェクトをコミット

---
+++ RubyTranslator/InputViewController.swift
@@ -0,0 +1,20 @@
+//
+//  InputViewController.swift
+//  RubyTranslator
+//
+//  Created by takanoyuki on 2020/03/14.
+//  Copyright © 2020 ytakano. All rights reserved.
+//
+
+import UIKit
+
+class InputViewController: UIInputViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
